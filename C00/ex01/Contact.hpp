6d5e08abfaa0d2d625af82650553e21849950a68
Cristiana nicoleta Marcu <cmarcu@c1r10s5.42madrid.com>
2022-11-22 15:55:15 +0100

    getters and setters

--- C00/ex01/Contact.hpp
+++ C00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 20:16:52 by cmarcu            #+#    #+#             */
-/*   Updated: 2022/11/03 20:36:42 by cmarcu           ###   ########.fr       */
+/*   Updated: 2022/11/22 15:54:42 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,18 @@ public:
 
     Contact();
     
-    int setFirstName(std::string name);
-    int getFirstName();
+    void setFirstName(std::string name);
+    void getFirstName();
     //Getters y setters para todos los atributos y desarrollarlos en el .cpp ¿?
-        
+    void setLastName(std::string lastName);
+    void getLastName();
+    
+    void setNickname(std::string nickname);
+    void getNickname();
+
+    void setPhoneNumber(std::string phone);
+    void getPhoneNumber();
+
+    void setDarkestSecret(std::string secret);
+    void getDarkestSecret();
 };

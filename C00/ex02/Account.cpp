e972f1cb000349de281fae5f734ef5220de9279d
Cristiana nicoleta Marcu <cmarcu@c3r1s3.42madrid.com>
2022-12-26 19:20:06 +0100

    pending withdrawal implementation

--- C00/ex02/Account.cpp
+++ C00/ex02/Account.cpp
@@ -9,8 +9,12 @@ int Account::_totalNbWithdrawals = 0;
 Account::Account(int initial_deposit)
 {
 	//[19920104_091532] index:0;amount:42;created
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 	_displayTimestamp();
-	std::cout << "Account created: " << initial_deposit << std::endl;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 	_nbAccounts++;
 }
 
@@ -42,12 +46,18 @@ int Account::getNbWithdrawals(void)
 void Account::displayAccountsInfos(void)
 {
 	//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
-	//[19920104_091532] index:0;amount:47;deposits:1;withdrawals:0
-	std::cout << "Deposit: " << deposit << std::endl;
+	//[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+	this->_nbDeposits++;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount << ";deposit:" << deposit;
+	this->_amount += deposit;
+	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
@@ -67,12 +77,20 @@ void Account::displayStatus(void) const
 {
 	//[19920104_091532] index:1;amount:54;deposits:0;withdrawals:0
 	//[19920104_091532] index:1;amount:785;deposits:1;withdrawals:1
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void Account::_displayTimestamp(void)
 {
 	//[19920104_091532]
-	std::time_t result = std::time(nullptr);
-    std::cout << std::asctime(std::localtime(&result))
-              << result << " seconds since the Epoch\n";
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer [80];
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	strftime (buffer,80,"[%Y%m%d_%H%M%S] ",timeinfo);
+	std::cout << buffer;
 }

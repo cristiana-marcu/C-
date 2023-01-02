ffd7f72d2c63330d932ec3268aaa38134caf69a6
Cristiana nicoleta Marcu <cmarcu@c3r1s3.42madrid.com>
2023-01-02 14:38:55 +0100

    saved from previous session

---
+++ C01/ex03/HumanA.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/30 13:33:06 by cmarcu            #+#    #+#             */
+/*   Updated: 2023/01/02 14:37:59 by cmarcu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef HUMAN_A_HPP
+#define HUMAN_A_HPP
+
+# include <iostream>
+# include "Weapon.hpp"
+
+class HumanA {
+
+private:
+    Weapon _weapon;
+    std::string _name;
+
+public:
+
+    HumanA( std::string name,  Weapon weapon );
+    
+    Weapon getWeapon( void );
+    void setWeapon( Weapon weapon);
+    
+    std::string getName( void );
+    void setName( std::string name);
+    
+    void attack( void );
+    
+};
+
+
+#endif

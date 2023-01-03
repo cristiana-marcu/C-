f34b5d8a87f75b8f784aebcc3093b62dc5ab2abc
Cristiana nicoleta Marcu <cmarcu@c3r14s4.42madrid.com>
2023-01-03 17:17:18 +0100

    open and create file

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,13 +6,37 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:10:54 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/01/03 14:19:02 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/01/03 17:16:15 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Harl.hpp"
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    if (argc != 4) {
+        std::cout << "Invalid number of arguments" << std::endl;
+        exit(1);
+    }
+    
+    std::string filename = argv[1];
+    std::ifstream ifs(filename, std::ifstream::in);
+    if (ifs.fail()) {
+        std::cout << "No such file" << std::endl;
+        exit(1);
+    }
+    
+    filename.append(".replace");
+    std::ofstream ofs(filename, std::ofstream::out);
+    if (ofs.fail()) {
+        std::cout << "Unable to create file" << std::endl;
+        exit(1);
+    }
+    ofs << ifs.rdbuf();
+    
+
+    ifs.close();
+    ofs.close();
     return 0;
 }

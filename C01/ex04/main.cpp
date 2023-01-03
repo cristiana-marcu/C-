404e19d9a933ba73ee52c05eb22c9d84de49ca09
Cristiana nicoleta Marcu <cmarcu@c3r14s4.42madrid.com>
2023-01-03 17:55:05 +0100

    maybe strinstreams are the keyyy

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,12 +6,14 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:10:54 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/01/03 17:16:15 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/01/03 17:44:56 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <iostream>
+#include <sstream> 
+#include <string>
 
 int main(int argc, char *argv[])
 {
@@ -34,8 +36,12 @@ int main(int argc, char *argv[])
         exit(1);
     }
     ofs << ifs.rdbuf();
-    
 
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    std::cout << ss.str() << std::endl;
+    std::string s = ss.str();
+    std::cout << s << std::endl;
     ifs.close();
     ofs.close();
     return 0;

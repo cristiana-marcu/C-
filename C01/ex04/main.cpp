8ff7a6495ed218760b50586031fa94aca68974d6
Cristiana nicoleta Marcu <cmarcu@c3r14s4.42madrid.com>
2023-01-03 21:51:32 +0100

    I think it works

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:10:54 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/01/03 17:44:56 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/01/03 21:49:43 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 #include <iostream>
 #include <sstream> 
 #include <string>
+
+void myReplace( std::string* buffer, std::string s1, std::string s2, int* pos ) {
+    *pos = (*buffer).find(s1, *pos);
+    if (*pos == -1)
+        return;
+    (*buffer).erase(*pos, s1.length());
+    (*buffer).insert(*pos, s2);
+    *pos += s2.length();
+}
 
 int main(int argc, char *argv[])
 {
@@ -35,13 +44,25 @@ int main(int argc, char *argv[])
         std::cout << "Unable to create file" << std::endl;
         exit(1);
     }
-    ofs << ifs.rdbuf();
 
-    std::stringstream ss;
-    ss << ifs.rdbuf();
-    std::cout << ss.str() << std::endl;
-    std::string s = ss.str();
-    std::cout << s << std::endl;
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+    std::cout << buffer.str() << std::endl;
+    
+    int lastCoincidence = 0;
+    std::string bufferStr = buffer.str();
+    
+    while (lastCoincidence != -1 && (size_t)lastCoincidence < bufferStr.length())
+    {
+        myReplace(&bufferStr, argv[2], argv[3], &lastCoincidence);
+        std::cout << bufferStr << std::endl;
+        std::cout << lastCoincidence << std::endl;
+        
+    }
+
+    std::cout << bufferStr << std::endl;
+    ofs << buffer.rdbuf();
+
     ifs.close();
     ofs.close();
     return 0;

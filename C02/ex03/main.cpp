9f1cd1f6dfc3815b01b0842de52bc4476600118b
Cristiana nicoleta Marcu <cmarcu@c3r10s2.42madrid.com>
2023-02-28 15:10:27 +0100

    Stuck on references

--- C02/ex03/main.cpp
+++ C02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 21:32:02 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/02/28 15:00:25 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/02/28 15:10:17 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 
 int main( void ) {
 
-    Point const a(1, 1);
-    std::cout << "Point X: " << a.getX() << std::endl;
-    Point const b(6, 1);
-    Point const c(3, 4);
-    Point const point(3.25, 2.8764);
+    Point const f(1, 1);
+    std::cout << "Point X: " << f.getX() << std::endl;
+    Point const g(6, 1);
+    Point const h(3, 4);
+    Point const pointM(3.25, 2.8764);
     Point const point2(7.0005, 2.8764);
     std::cout << "Point2 Y: " << point2.getY() << std::endl;
     Point const point3(3, 4);
 
-    bool result = bsp(a, b, c, point);
+    bool result = bsp(f, g, h, pointM);
     std::cout << "The point is in the triangle: " << result << std::endl;
-    result = bsp(a, b, c, point2);
+    result = bsp(f, g, h, point2);
     std::cout << "The point is in the triangle: " << result << std::endl;
-    result = bsp(a, b, c, point3);
+    result = bsp(f, g, h, point3);
     std::cout << "The point is in the triangle: " << result << std::endl;
 
     result = bsp( Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 10) );

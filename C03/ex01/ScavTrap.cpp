79918153add8173e6079dabb6c197e8c44434a86
Cristiana nicoleta Marcu <cmarcu@c2r17s3.42madrid.com>
2023-03-04 14:00:16 +0100

    inheritance started

--- C03/ex01/ScavTrap.cpp
+++ C03/ex01/ScavTrap.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 17:48:08 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/03/04 13:51:35 by cmarcu           ###   ########.fr       */
+/*   Created: 2023/03/04 13:58:12 by cmarcu            #+#    #+#             */
+/*   Updated: 2023/03/04 13:58:13 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
-
-int main( void ) {
-    
-    ClapTrap pavon("Pavon");
-    ClapTrap jalva("Jalva");
-    ClapTrap anonym;
-
-    pavon.attack("Jalva");
-    pavon.takeDamage(5);
-    pavon.beRepaired(3);
-    
-    anonym.attack("Pavon");
-
-    return 0;
-}
--- C03/ex01/ScavTrap.hpp
+++ C03/ex01/ScavTrap.hpp
@@ -1,28 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 17:48:08 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/03/04 13:51:35 by cmarcu           ###   ########.fr       */
+/*   Created: 2023/03/04 13:58:10 by cmarcu            #+#    #+#             */
+/*   Updated: 2023/03/04 13:58:55 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#pragma once
 
-int main( void ) {
-    
-    ClapTrap pavon("Pavon");
-    ClapTrap jalva("Jalva");
-    ClapTrap anonym;
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
 
-    pavon.attack("Jalva");
-    pavon.takeDamage(5);
-    pavon.beRepaired(3);
-    
-    anonym.attack("Pavon");
 
-    return 0;
-}
+
+#endif

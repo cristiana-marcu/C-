83164c2852ff7c70c2172d27af4c4ca1ea78ae13
cristiana-marcu <cristiana.marcu.n@gmail.com>
2023-06-16 16:15:20 +0200

    character half way there

--- C04/ex03/Character.cpp
+++ C04/ex03/Character.cpp
@@ -6,7 +6,42 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 21:05:32 by cristianama       #+#    #+#             */
-/*   Updated: 2023/06/12 21:05:33 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/06/16 12:53:24 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Character.hpp"
+
+Character::Character( std::string const & name ) : _name(name)
+{
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = NULL;
+	std::cout << "Character " << this->_name << " created." << std::endl;
+}
+
+Character::Character( Character const & rhs) : _name(rhs.getName())
+{
+	*this = rhs;
+}
+
+Character& Character::operator=( Character const & rhs)
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs.getName();
+		for (int i = 0; i < 4; i++)
+			this->_inventory[i] = rhs._inventory[i];
+	}
+	return *this;
+}
+
+Character::~Character() 
+{
+	for (int i = 0; i < 4; i++)
+		if (this->_inventory[i])
+			delete [] _inventory[i];
+	std::cout << "Character " << this->_name << " destroyed" << std::endl;
+}
+
+std::string const & Character::getName() const { return this->_name; }
 
--- C04/ex03/Character.hpp
+++ C04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 21:06:02 by cristianama       #+#    #+#             */
-/*   Updated: 2023/06/12 21:09:46 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/06/16 12:36:56 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 
 class Character : public ICharacter
 {
+protected:
+	std::string _name;
+	AMateria *_inventory[4];
+	
 public:
 	
-	Character();
+	Character( std::string const & name );
 	Character( Character const & rhs );
-	Character & operand=( Character const & rhs );
+	Character & operator=( Character const & rhs );
 	~Character();
 	
-	std::string const & getName() const = 0;
-	void equip(AMateria* m) = 0;
-	void unequip(int idx) = 0;
-	void use(int idx, ICharacter& target) = 0;
+	std::string const & getName() const override;
+	void equip(AMateria* m) override;
+	void unequip(int idx) override;
+	void use(int idx, ICharacter& target) override;
 };

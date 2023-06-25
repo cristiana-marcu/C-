431666d62fbf96b57a5a034675d0a4ccfc1cba1a
cristiana-marcu <cristiana.marcu.n@gmail.com>
2023-06-26 00:13:03 +0200

    ex00 done

--- C05/ex00/Bureaucrat.hpp
+++ C05/ex00/Bureaucrat.hpp
@@ -6,11 +6,9 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 09:18:23 by cristianama       #+#    #+#             */
-/*   Updated: 2023/06/25 09:33:23 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/06/26 00:12:19 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-class Bureaucrat;
 
 #pragma once
 # include <iostream>
@@ -19,17 +17,38 @@ class Bureaucrat
 {
 private:
 	const std::string _name;
-	int range;
+	int _range;
 	
 public:
 	Bureaucrat();
-	Bureaucrat( std::string name );
+	Bureaucrat( std::string name, int grade );
 	Bureaucrat( Bureaucrat const &src );
 	Bureaucrat & operator=( Bureaucrat const &rhs );
 	~Bureaucrat();
 
-	std::string getName();
-	int getRange();
+	std::string getName() const;
+	int getRange() const;
 	void incrementRange(int amount);
 	void decrementRange(int amount);
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return "Grade is too high";
+		}
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return "Grade is too low";
+		}
+	};
+
 };
+
+std::ostream& operator<<( std::ostream& o, const Bureaucrat& rhs);

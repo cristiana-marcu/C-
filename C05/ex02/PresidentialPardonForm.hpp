f85850fce9141372b8a08abf9d402d358f3d78b8
cristiana-marcu <cristiana.marcu.n@gmail.com>
2023-06-27 23:13:22 +0200

    ex02 done

--- C05/ex02/PresidentialPardonForm.hpp
+++ C05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 15:31:32 by cristianama       #+#    #+#             */
-/*   Updated: 2023/06/27 15:32:24 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/06/27 23:07:41 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,23 @@
  Required grades: sign 25, exec 5
 Informs that <target> has been pardoned by Zaphod Beeblebrox.
 */
+
+#pragma once
+# include <iostream>
+# include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+private:
+	std::string _target;
+	PresidentialPardonForm();
+public:
+	PresidentialPardonForm( std::string const target );
+	PresidentialPardonForm( PresidentialPardonForm const &ref );
+	PresidentialPardonForm& operator=( PresidentialPardonForm const &rhs );
+	~PresidentialPardonForm();
+	
+	std::string getTarget() const;
+	
+	void execute( const Bureaucrat & executor ) const;
+
+};
--- C05/ex02/main.cpp
+++ C05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:48:08 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/06/27 22:59:43 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/06/27 23:12:24 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 #include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main( void ) {
 
 	try
 	{
-		Bureaucrat pepe("Pepe", 10);
+		Bureaucrat pepe("Pepe", 109);
 		ShrubberyCreationForm tree("little_tree");
 		RobotomyRequestForm robot("bibobimo");
+		PresidentialPardonForm pardon("Crisi");
 		
 		pepe.signForm(tree);
 		pepe.executeForm(tree);
 
 		pepe.signForm(robot);
 		pepe.executeForm(robot);
+
+		pepe.signForm(pardon);
+		pepe.executeForm(pardon);
 	}
 	catch (std::exception &e)
 	{

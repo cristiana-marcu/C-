fa844a45e63f2ed93699e8b110ed121dcb88e644
cristiana-marcu <cristiana.marcu.n@gmail.com>
2023-07-16 22:24:53 +0200

    base classes for ex02 generated

---
+++ C06/ex02/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 17:48:08 by cmarcu            #+#    #+#             */
+/*   Updated: 2023/07/16 22:21:50 by cristianama      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+int main( void ) {
+
+	
+	
+	return 0;
+}

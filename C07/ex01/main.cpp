ff84b704393eb5a5896797b10453513969ed4732
Cristiana nicoleta Marcu <cmarcu@c1r9s6.42madrid.com>
2023-07-24 15:02:01 +0200

    more tests on main

--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -6,45 +6,24 @@
 /*   By: cmarcu <cmarcu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 15:37:46 by cmarcu            #+#    #+#             */
-/*   Updated: 2023/07/24 14:48:19 by cmarcu           ###   ########.fr       */
+/*   Updated: 2023/07/24 15:00:54 by cmarcu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
 
-// void printInt(int x)
-// {
-// 	std::cout << x << std::endl;
-// }
-
-// int main() {
-// 	int array[] = {1, 2, 3, 4, 5};
-// 	size_t length = sizeof(array) / sizeof(int);
-
-// 	iter(array, length, printInt);
-
-// 	return 0;
-// }
-
-class Awesome
+void printInt(int const &x)
 {
-	public:
-	Awesome( void ) : _n( 42 ) { return; }
-	int get( void ) const { return this->_n; }
-	private:
-	int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
-
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
+	std::cout << x << std::endl;
+}
 
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 };
-	Awesome tab2[5];
-	::iter( tab, 5, print );
-	::iter( tab2, 5, print );
+	int array[] = {1, 2, 3, 4, 5};
+	size_t length = 5;
+
+	::iter(array, length, printInt);
+
 	return 0;
 }
+

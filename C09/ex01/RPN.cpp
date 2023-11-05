177d47fa9c3857307ce66b1a499e55cf61b861f2
cristiana-marcu <cristiana.marcu.n@gmail.com>
2023-11-05 15:23:09 +0100

    better error handling

--- C09/ex01/RPN.cpp
+++ C09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 16:54:16 by cristianama       #+#    #+#             */
-/*   Updated: 2023/10/22 15:32:02 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/11/05 15:17:57 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,57 +19,56 @@ RPN::~RPN() {}
 RPN::RPN(const RPN& other) : s(other.s) {}
 
 RPN& RPN::operator=(const RPN& other) {
-    if (this != &other) {
-        s = other.s;
-    }
-    return *this;
+	if (this != &other) {
+		s = other.s;
+	}
+	return *this;
 }
 
 int RPN::eval(const std::string &exp) {
-    std::stringstream ss(exp);
-    std::string token;
-	
-	// Clear the stack for a new evaluation
-    while (!s.empty()) {
-        s.pop();
-    }
+	std::stringstream ss(exp);
+	std::string token;
 
-    while (ss >> token) {
-        if (token == "+") {
-            if (s.size() < 2) return -1; // Error
-            int b = s.top(); s.pop();
-            int a = s.top(); s.pop();
-            s.push(a + b);
-        } else if (token == "-") {
-            if (s.size() < 2) return -1; // Error
-            int b = s.top(); s.pop();
-            int a = s.top(); s.pop();
-            s.push(a - b);
-        } else if (token == "*") {
-            if (s.size() < 2) return -1; // Error
-            int b = s.top(); s.pop();
-            int a = s.top(); s.pop();
-            s.push(a * b);
-        } else if (token == "/") {
-            if (s.size() < 2) return -1; // Error
-            int b = s.top(); s.pop();
-            if (b == 0) return -1; // Divide by zero error
-            int a = s.top(); s.pop();
-            s.push(a / b);
-        } else { // Number or invalid token
-            try {
-                int num = std::stoi(token);
-                if (num >= 0 && num < 10) {
-                    s.push(num);
-                } else {
-                    return -1; // Invalid number
-                }
-            } catch (const std::invalid_argument&) {
-                return -1; // Invalid token
-            }
-        }
-    }
+	while (!s.empty()) {
+		s.pop();
+	}
 
-    if (s.size() != 1) return -1; // Error
-    return s.top();
+	while (ss >> token) {
+		if (token == "+") {
+			if (s.size() < 2) return -1; // Error
+			int b = s.top(); s.pop();
+			int a = s.top(); s.pop();
+			s.push(a + b);
+		} else if (token == "-") {
+			if (s.size() < 2) return -1; // Error
+			int b = s.top(); s.pop();
+			int a = s.top(); s.pop();
+			s.push(a - b);
+		} else if (token == "*") {
+			if (s.size() < 2) return -1; // Error
+			int b = s.top(); s.pop();
+			int a = s.top(); s.pop();
+			s.push(a * b);
+		} else if (token == "/") {
+			if (s.size() < 2) return -1; // Error
+			int b = s.top(); s.pop();
+			if (b == 0) return -1; // Divide by zero error
+			int a = s.top(); s.pop();
+			s.push(a / b);
+		} else { // Number or invalid token
+			try {
+				int num = std::stoi(token);
+				if (num >= 0 && num < 10) {
+					s.push(num);
+				} else {
+					return -1;
+				}
+			} catch (const std::invalid_argument&) {
+				return -1;
+			}
+		}
+	}
+
+	if (s.size() != 1) return -1;
+	return s.top();
 }
--- C09/ex01/main.cpp
+++ C09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 16:53:56 by cristianama       #+#    #+#             */
-/*   Updated: 2023/10/22 15:29:27 by cristianama      ###   ########.fr       */
+/*   Updated: 2023/11/05 15:18:13 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " \"expression\"" << std::endl;
-        return 1;
-    }
+		std::cout << "Usage: " << argv[0] << " \"expression\"" << std::endl;
+		return 1;
+	}
 
 	RPN rpn;
-    int result = rpn.eval(argv[1]);
-    if (result == -1) {
-        std::cout << "Error" << std::endl;
-    } else {
-        std::cout << result << std::endl;
-    }
+	int result = rpn.eval(argv[1]);
+	if (result == -1) {
+		std::cout << "Error" << std::endl;
+	} else {
+		std::cout << result << std::endl;
+	}
 
 	return 0;
 }
